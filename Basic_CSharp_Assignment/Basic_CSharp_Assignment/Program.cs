116b8963a518b7d8dea807fd002ffde19254eecd
David Buckingham <davidbuckingham95@gmail.com>
2021-08-18 15:54:07 +0100

    Committing Basic C# Assignment
    Adding Basic_CSharp_Assignment folder

---
+++ Basic_CSharp_Assignment/Basic_CSharp_Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basic_CSharp_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
+        }
+    }
+}
