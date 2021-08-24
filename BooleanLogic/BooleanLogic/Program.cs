38451f936bae54144500589313098e5ecac29e8d
David Buckingham <davidbuckingham95@gmail.com>
2021-08-24 15:32:12 +0100

    Committing Boolean logic Assignment
    Adding BooleanLogic solution

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI violation?(please enter true or false)");
+            bool violation = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you had?");
+            int numOfTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && violation == false && numOfTickets <= 3;
+            Console.WriteLine(qualified);
+            Console.Read();
+        }
+    }
+}
