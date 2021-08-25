618285490ed52241eae017506523e52590981a3e
David Buckingham <davidbuckingham95@gmail.com>
2021-08-25 13:40:50 +0100

    Committing Loops Assignment
    Adding Loops solution

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isGuessed = false;
+            int number;
+            bool guess = false;
+            Random rnd = new Random();
+            int randomNum = rnd.Next(1, 100);
+            
+
+            while (isGuessed == false)
+            {
+                Console.WriteLine("Pick a number between 1 and 100:");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 57)
+                {
+                    Console.WriteLine("You guessed correctly the number was 57");
+                    isGuessed = true;
+                }
+                else
+                {
+                    Console.WriteLine("you guessed wrong try again");
+                }
+            }
+
+            do
+            {
+                Console.WriteLine("Pick a number between 1 and 100:");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == randomNum)
+                {
+                    Console.WriteLine("You guessed correctly the number was " + randomNum);
+                    guess = true;
+                }
+                else
+                {
+                    Console.WriteLine("you guessed wrong try again");
+                }
+            }
+
+            while (!guess && isGuessed);
+
+        }
+    }
+}
