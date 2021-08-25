017a7472dff760e7d0f56c79607606860ec28a82
David Buckingham <davidbuckingham95@gmail.com>
2021-08-25 14:05:21 +0100

    Committing String Assignment
    Adding String solution

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string opening = "Hi my name is David ";
+            string middle = "I am 26 years old ";
+            string end = "and I am a student of Pitman Training";
+            string sentence = string.Concat(opening, middle, end);
+
+            string testString = "converted to upper case";
+            testString.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is David.");
+            sb.Append("I am 26 years old.");
+            sb.Append("I am a student of Pitman Training.");
+            sb.Append("I am currently studying for my advanced software and web development diploma.");
+
+            Console.WriteLine(sentence);
+            Console.WriteLine(testString);
+            Console.WriteLine(sb);
+        }
+    }
+}
