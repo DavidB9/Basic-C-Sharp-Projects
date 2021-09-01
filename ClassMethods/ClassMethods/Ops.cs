24d0345deeec262c0f07b6d1ae940dbabbbb0628
David Buckingham <davidbuckingham95@gmail.com>
2021-09-01 16:27:19 +0100

    Committing ClassMethods Assignment
    Adding ClassMethods solution

---
+++ ClassMethods/ClassMethods/Ops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethods
+{
+    class Ops
+    {
+        public void div(int x)
+        {
+            x = x / 2;
+            Console.WriteLine("Result is: " + x);
+        }
+
+        public void outParameters(out int x)
+        {
+            int temp = 5;
+            x = temp;
+        }
+        public decimal div(decimal x)
+        {
+            x = x / 2;
+            return x;
+        }
+    }
+}
---
+++ ClassMethods/ClassMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ops testOp = new Ops();
+
+            Console.WriteLine("Please enter a number to be divided by 2:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            testOp.div(num);
+
+            int a = 100;
+            testOp.outParameters(out a);
+            Console.WriteLine("a was 100 and is now: " + a);
+
+            //overloaded method
+            decimal result = testOp.div(50.2m);
+            Console.WriteLine("Overloaded method returns: " + result);
+
+            Console.Read();
+        }
+    }
+}
