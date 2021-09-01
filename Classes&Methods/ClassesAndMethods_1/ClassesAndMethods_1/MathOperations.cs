cd65beeb9ce978ded2faf4f9fecbd295e5df98af
David Buckingham <davidbuckingham95@gmail.com>
2021-09-01 15:31:36 +0100

    Committing ClassesAndMethods_1
    Adding ClassesAndMethods_1 solution

---
+++ Classes&Methods/ClassesAndMethods_1/ClassesAndMethods_1/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndMethods_1
+{
+    class MathOperations
+    {
+        public int Add(int x)
+        {
+            x = x + 5;
+            return x;
+        }
+        public decimal Add(decimal x)
+        {
+            x = x + 5.5m;
+            return x;
+        }
+        public int Add(string x)
+        {
+            int  y = Convert.ToInt32(x);
+            y += 10;
+            return y;
+        }
+    }
+}
---
+++ Classes&Methods/ClassesAndMethods_1/ClassesAndMethods_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesAndMethods_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOps = new MathOperations();
+            int result = mathOps.Add(3);
+            decimal result2 = mathOps.Add(5.6m);
+            int result3 = mathOps.Add("6");
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.Read();
+        }
+    }
+}
