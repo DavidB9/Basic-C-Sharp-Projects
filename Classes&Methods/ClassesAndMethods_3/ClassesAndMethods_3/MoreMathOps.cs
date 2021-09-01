01b984a4c216ab7a5beaa18f7929ef6bcaa71bff
David Buckingham <davidbuckingham95@gmail.com>
2021-09-01 16:01:49 +0100

    Committing ClassesAndMethods_3
    Adding ClassesAndMethods_3 solution

---
+++ Classes&Methods/ClassesAndMethods_3/ClassesAndMethods_3/MoreMathOps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndMethods_3
+{
+    class MoreMathOps
+    {
+        public void test(int x, int y)
+        {
+            x = x + 10;
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ Classes&Methods/ClassesAndMethods_3/ClassesAndMethods_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesAndMethods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoreMathOps testOps = new MoreMathOps();
+            testOps.test(5,5);
+            testOps.test(x: 6, y: 10);
+            Console.Read();
+        }
+    }
+}
