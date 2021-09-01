96a29c27b7b019bdf42f5b538cd492057eda366b
David Buckingham <davidbuckingham95@gmail.com>
2021-09-01 15:02:29 +0100

    Committing Methods Assignment
    Adding Methods Solution

---
+++ Methods/Methods/MathOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class MathOps
+    {
+        public int add(int x)
+        {
+            x = x + 10;
+            return x;
+        }
+
+        public int sub(int y)
+        {
+            y = y - 1;
+            return y;
+        }
+
+        public int multiply(int z)
+        {
+            z = z * 5;
+            return z;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an instance of the MathOps class
+            MathOps testMath = new MathOps();
+
+            //ask the user for a number
+            Console.WriteLine("Please enter a number to do the math operations on:");
+
+            //save the number as a variable
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //save the three results of the class methods to variables
+            int result1 = testMath.add(num);
+            int result2 = testMath.sub(num);
+            int result3 = testMath.multiply(num);
+
+            //Display the results on the console
+            Console.WriteLine("Add 10 " + result1);
+            Console.WriteLine("Subtract by 1 " + result2);
+            Console.WriteLine("Multiply by 5 " + result3);
+
+        }
+    }
+}
