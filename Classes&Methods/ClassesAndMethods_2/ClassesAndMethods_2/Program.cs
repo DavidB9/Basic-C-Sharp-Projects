d2efd015f435bfaa276f7c07ec304b2badabfc58
David Buckingham <davidbuckingham95@gmail.com>
2021-09-01 15:51:29 +0100

    Committing ClassesAndMethods_2
    Adding ClassesAndMethods_2 solution

---
+++ Classes&Methods/ClassesAndMethods_2/ClassesAndMethods_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassesAndMethods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test testOp = new test();
+            int result;
+            Console.WriteLine("Please enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number(optional)");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                result = testOp.outPara(num1, num2);
+                Console.WriteLine("Result: " + result);
+            }
+            catch(Exception ex)
+            {
+                result = testOp.outPara(num1);
+                Console.WriteLine("Result: " + result);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Classes&Methods/ClassesAndMethods_2/ClassesAndMethods_2/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndMethods_2
+{
+    class test
+    {
+        public int outPara(int x,  int y = 6)
+        {
+            x = x + y;
+            return x;
+        }
+    }
+}
