0c2b20d3469670146a806836618a39f862b20723
David Buckingham <davidbuckingham95@gmail.com>
2021-09-06 13:08:05 +0100

    Committing Operator Overloading Assignment
    Added overloaded operator to Abstract solution

--- Abstract/Abstract/Employee.cs
+++ Abstract/Abstract/Employee.cs
@@ -6,6 +6,8 @@ namespace Abstract
 {
     class Employee : Person , IQuittable
     {
+        public int Id { get; set; }
+
         public override void SayName()
         {
             Console.WriteLine("Name: " + FirstName + " " + LastName);
@@ -14,6 +16,16 @@ namespace Abstract
         public void Quit()
         {
             Console.WriteLine("Employee Quit");
+        }
+
+        public static bool operator==(Employee em, Employee em2)
+        {
+            return em.Id == em2.Id;
+        }
+
+        public static bool operator !=(Employee em, Employee em2)
+        {
+            return em.Id == em2.Id;
         }
     }
 }
--- Abstract/Abstract/Program.cs
+++ Abstract/Abstract/Program.cs
@@ -6,13 +6,22 @@ namespace Abstract
     {
         static void Main(string[] args)
         {
+            bool equal = false;
             Employee emp = new Employee();
             emp.FirstName = "Sample";
             emp.LastName = "Student";
+            emp.Id = 5;
             emp.SayName();
             IQuittable test = new Employee();
+
             test.Quit();
+            Employee emp2 = new Employee();
+            emp2.Id = 6;
+            equal = emp == emp2;
+            Console.WriteLine(equal);
             Console.Read();
         }
+
+
     }
 }
