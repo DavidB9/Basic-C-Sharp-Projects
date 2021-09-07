c2a6bbe4984dd8fd6928fee5e05dc83298d68de0
David Buckingham <davidbuckingham95@gmail.com>
2021-09-07 13:11:21 +0100

    Committing Generics Assignment
    Adding Generics solution

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Employee<T> : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public List<T> things { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Employee Quit");
+        }
+
+        public void printThings()
+        {
+            for(int i =0; i < things.Count; i++)
+            {
+                Console.WriteLine(things[i]);
+            }
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp1 = new Employee<string>();
+            emp1.things = new List<string>();
+            emp1.things.Add("Things");
+            emp1.things.Add("Stare");
+            emp1.things.Add("Lot");
+            emp1.things.Add("Sting");
+            emp1.things.Add("Orange");
+
+            Employee<int> emp2 = new Employee<int>();
+            emp2.things = new List<int>();
+            emp2.things.Add(5);
+            emp2.things.Add(9);
+            emp2.things.Add(7);
+            emp2.things.Add(6);
+            emp2.things.Add(10);
+
+            emp1.printThings();
+            emp2.printThings();
+
+            Console.Read();
+        }
+    }
+}
