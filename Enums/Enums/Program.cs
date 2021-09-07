b5e9ed82d634cbaba15c01bb0e9bc8b0a762232f
David Buckingham <davidbuckingham95@gmail.com>
2021-09-07 13:30:35 +0100

    Committing Enums Assignment
    Adding Enums solution

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week:");
+            try
+            {
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine(), true);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            Console.Read();
+
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
