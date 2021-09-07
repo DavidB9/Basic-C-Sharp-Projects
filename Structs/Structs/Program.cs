cd48fe6d7c147b7c6bf09a2cf7404dbb53f98fef
David Buckingham <davidbuckingham95@gmail.com>
2021-09-07 13:45:45 +0100

    Committing Struct Assignment
    Adding Struct solution

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1;
+            num1.Amount = 50.6m;
+            Console.WriteLine(num1.Amount);
+            Console.Read();
+        }
+    }
+}
