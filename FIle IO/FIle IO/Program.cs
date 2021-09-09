3778244b0bd1ad875df11d3b10ab8886bcd67a36
David Buckingham <davidbuckingham95@gmail.com>
2021-09-09 14:13:13 +0100

    Committing File I/O Assignment
    Adding file I/O solution

---
+++ FIle IO/FIle IO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FIle_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string number = Console.ReadLine();
+            File.WriteAllText("file.text", number);
+            Console.WriteLine(File.ReadAllText("file.text"));
+            Console.Read();
+        }
+    }
+}
