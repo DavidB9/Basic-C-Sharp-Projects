c82e4e774d8854b083e3cb0140c92c8d3c94a264
David Buckingham <davidbuckingham95@gmail.com>
2021-09-09 14:30:36 +0100

    Committing DateTime Assignment
    Adding Date solution

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+            Console.WriteLine("Please enter the number of hours");
+            int num = Convert.ToInt32(Console.ReadLine());
+            date = date.AddHours(num);
+            Console.WriteLine(date);
+            Console.Read();
+        }
+    }
+}
