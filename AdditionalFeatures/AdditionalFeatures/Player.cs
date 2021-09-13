c63d56afaf17253b084e2977b77c4fdf466f9e73
David Buckingham <davidbuckingham95@gmail.com>
2021-09-13 14:27:41 +0100

    Committing Additional Features Assignment
    Adding AdditionFeatures  solution

---
+++ AdditionalFeatures/AdditionalFeatures/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdditionalFeatures
+{
+    public class Player
+    {
+        public int playerHealth { get; set; }
+        public string playerName { get; set; }
+
+        public Player(string name) : this(name, 100)
+        {
+
+        }
+
+        public Player(string name, int health)
+        {
+            playerHealth = health;
+            playerName = name;
+        }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string message = "Welcome";
+            Console.WriteLine(message);
+            var player = new Player("David", 75);
+            var player2 = new Player("Cian");
+
+            Console.WriteLine(player.playerHealth);
+            Console.WriteLine(player2.playerHealth);
+            Console.Read();
+        }
+    }
+}
